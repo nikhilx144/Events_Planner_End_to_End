542f1e062945a46af9ac9094fd735a86ed0d63a5
Nikhil <nikhilgelli1@gmail.com>
2025-11-24 12:01:48 +0530

    changed the duration from 5s to 60s

--- terraform/delay.tf
+++ terraform/delay.tf
@@ -1,8 +1,8 @@
-resource "time_sleep" "wait_for_gateway" {
-    create_duration = "5s"
+resource "time_sleep" "wait_for_methods" {
+  depends_on = [
+    aws_api_gateway_method.signup_method,
+    aws_api_gateway_method.login_method
+  ]
 
-    depends_on = [
-        aws_api_gateway_method.signup_method,
-        aws_api_gateway_method.login_method,
-    ]
+  create_duration = "60s"
 }
