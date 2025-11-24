45a4316c7ad82469360dafa75f9583462dd89631
Nikhil <nikhilgelli1@gmail.com>
2025-11-24 15:14:22 +0530

    fixed the recurring enumeration error

--- terraform/delay.tf
+++ terraform/delay.tf
@@ -1,8 +1,25 @@
+########################################
+# Delay AFTER methods, BEFORE integrations
+########################################
+
 resource "time_sleep" "wait_for_methods" {
   depends_on = [
     aws_api_gateway_method.signup_method,
-    aws_api_gateway_method.login_method
+    aws_api_gateway_method.login_method,
+    aws_api_gateway_method.signup_options
   ]
 
-  create_duration = "60s"
+  create_duration = "10s"
+}
+
+########################################
+# Delay AFTER REST API, BEFORE resources
+########################################
+
+resource "time_sleep" "wait_for_rest_api" {
+  depends_on = [
+    aws_api_gateway_rest_api.auth_api
+  ]
+
+  create_duration = "20s"
 }
